b600db256efa396266659b2977ae2b71de42cb40
Hardik Patel <hardikgw@gmail.com>
2017-07-13 12:20:48 -0400

    comamnds

--- src/main/resources/commands.sh
+++ src/main/resources/commands.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker run -it --name=spark -p 7777:7777 -p 8888:8888 -p 9000:9000 -p 8088:8088 -p 8031:8031 -p 8032:8032 -p 8033:8033 -p 50070:50070 -p 50075:50075 -p 50030:50030 -v $(pwd)/data:/data cithub/spark-yarn
+docker run -it --name=spark -p 4040:4040 -p 8888:8888 -p 9000:9000 -p 8088:8088 -p 8031:8031 -p 8032:8032 -p 8033:8033 -p 50070:50070 -p 50075:50075 -p 50030:50030 -v $(pwd)/data:/data cithub/spark-yarn
 # following inside giraph container
 hdfs namenode -format
 ./home/hadoop/hadoop/start.sh
@@ -12,8 +12,9 @@ hdfs dfs -ls /tmp
 export SPARK_HOME=/home/hadoop/spark/spark
 export YARN_CONF_DIR=/home/hadoop/hadoop/etc/hadoop
 export PYSPARK_DRIVER_PYTHON=ipython3
-export PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --port=8888"
+export PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --port=7777"
 export PYSPARK_PYTHON=python3
+
 
 cd $SPARK_HOME
 ./bin/spark-submit --class org.apache.spark.examples.SparkPi \
@@ -31,3 +32,5 @@ jupyter notebook --port=8888 --no-browser --ip=0.0.0.0 --allow-root
 
 docker exec spark hdfs dfs -mkdir /data
 docker exec spark hdfs dfs -put data/yago/yagoFacts.tsv /data/yagoFacts.tsv
+
+# folder for jupyter kernel config /usr/local/share/jupyter/kernels/apache_toree_scala/kernel.json
